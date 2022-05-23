65d8b11f27cc7e6bc088661af43f2b61c0be63ef
rageoveralostpenny <wu.cindyx@gmail.come>
2022-05-23 11:05:56 +0100

    Exercise 2 but wrong values?

--- Boundary Layer/Exercise2.m
+++ Boundary Layer/Exercise2.m
@@ -1,15 +1,25 @@
-x = linspace(0,1,101);
-ue = ones(101,3);
+clc
+clear all
+
+n = 101;
+% In all following code, we work with the 1st dimension or column dimension
+% as n (linspace creates row vectors, so transpose)
+x = linspace(0,1,n)';
+% 101x3 because each column is for a different gradient
+ue = ones(n,3);
 gradients = [-0.1,0,0.1];
 Re = [5*10^6, 10^7, 2*10^7];
 % First dimension is different x/L values
 % Second dimension is different gradients
 % Third dimension is different Re values
-f = zeros(101,3,3);
-theta = zeros(101,3,3);
-m = zeros(101,3,3);
+theta = zeros(n,3,3);
+f = zeros(n,3,3);
+m = zeros(n,3,3);
 % Local Reynolds number based on momentum thickness
-Re_theta = zeros(101,3,3);
+Re_theta = zeros(n,3,3);
+% Final 3*3 matrices to output transition values
+transition_loc = zeros(3);
+transition_Re = zeros(3);
 
 % Fill ue
 % Each column of ue is for a different value of gradient wrt dimensionless
@@ -28,40 +38,39 @@ end
 % i loops through distance along plate x/L
 for j=1:3
     for k=1:3
-        for i=2:101
-            f(i,j,k) = f(i-1,j,k)+(ue(i,j))^(-6)*ueintbit(x(i-1),ue(i-1,j),x(i),ue(i,j));
+        for i=2:n
+            f(i,j,k) = f(i-1,j,k)+ue(i,j)^(-6)*ueintbit(x(i-1),ue(i-1,j),x(i),ue(i,j));
         end
-        theta(:,j,k) = sqrt((0.45/Re(k))*f);
+        theta(:,j,k) = sqrt( (0.45/Re(k)) * f(:,j,k) );
         m(:,j,k) = -Re(k)*theta(:,j,k).^2*gradients(j);
         
-        for i=1:101
-             Re_theta = Re(k)*ue(i,j)*
+        for i=1:n
+            Re_theta(i,j,k) = Re(k)*ue(i,j)*theta(i,j,k);
+        end
     end
 end
 
-
 % Calculate H
-H = arrayfun(thwaites_lookup,m);
+H = arrayfun(@thwaites_lookup,m);
 
 % Calculate He
-He = arrayfun(laminar_He,m);
+He = arrayfun(@laminar_He,H);
 
+% Find transition point
+laminar = true;
 for j=1:3
     for k=1:3
-        for i=1:101
-            if log(Re_theta(i,j,k)) >= 18.4*He(i,j,k)
+        for i=1:n
+            if log(Re_theta(i,j,k)) >= 18.4*He(i,j,k)-21.74
                 laminar = false;
-                disp([x(i) Re_theta_1000])
+                disp([x(i) Re_theta(i,j,k)/1000])
+                transition_loc(j,k) = x(i);
+                transition_Re(j,k) = Re_theta(i,j,k);
                 break
             end
         end
     end
 end
 
-figure
-hold on
-plot(x, theta, 'r--');
-plot(x, blasius);
-legend('Thwaites','Blasius')
-xlabel('x/L')
-ylabel('theta/L')
+disp(transition_loc)
+disp(transition_Re)
