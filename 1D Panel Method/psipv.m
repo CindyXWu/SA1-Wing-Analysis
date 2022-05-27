5175a8384f417caf31489f61fff3cfae33baf266
ytl34 <ytl34@cam.ac.uk>
2022-05-27 15:25:49 +0100

    Updated

--- 1D Panel Method/psipv.m
+++
@@ -1,4 +0,0 @@
-function psixy = psipv(xc,yc,Gamma,x,y)
-    psixy = -Gamma*log((x-xc)^2+(y-yc)^2)/(4*pi);
-end
-
--- Boundary Layer/ueintbit.m
+++
@@ -1,6 +0,0 @@
-function f = ueintbit(xa,ua,xb,ub)
-    ubar = 0.5*(ua+ub);
-    delta_u = ub - ua;
-    delta_x = xb - xa;
-    f = (ubar^5+5/6*ubar^3*delta_u^2+1/16*ubar*delta_u^4)*delta_x;
-end
