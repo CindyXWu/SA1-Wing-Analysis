4b4e97800ddcfcd8b78e013053b0bc61221924b9
Cindy Wu <53667072+rageoveralostpenny@users.noreply.github.com>
2022-05-29 14:20:33 +0100

    Add files via upload

---
+++ Boundary Layer/Exercise1.m
@@ -0,0 +1,21 @@
+x = linspace(0,1,101);
+ue = ones(101,1);
+Re=1000;
+f = zeros(101,1);
+for i=2:101
+    f(i) = f(i-1)+(ue(i))^(-6)*ueintbit(x(i-1),ue(i-1),x(i),ue(i));
+end
+
+theta = sqrt((0.45/Re)*f);
+
+blasius = 0.664*sqrt(x)/(Re^(0.5));
+f_int = sum(f);
+
+disp(theta)
+figure
+hold on
+plot(x, theta, 'r--');
+plot(x, blasius);
+legend('Thwaites','Blasius')
+xlabel('x/L')
+ylabel('theta/L')
